c90cd2ac9737ac0619cc871f70613271d05153aa
Cassi <120105652+CypherO2@users.noreply.github.com>
2024-05-28 22:23:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import pygame
 import time
 import random
 
+# PYGAME INITIALISATION (?)
+pygame.init()
+
 # FONT INITIALISATION
 pygame.font.init()
 
 WIDTH, HEIGHT = 1600, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Test Game 1")
+pygame.display.set_caption("Asteroid Evaders")
+
+# GAME ICON
+icon = pygame.image.load("./assets/SpaceSmall-2.png").convert()
+pygame.display.set_icon(icon)
+
+# TITLE WIN - GLOBAL
+game_state = "start_menu"
+
 
 # Define Background Image
 BG = pygame.transform.scale(
@@ -15,6 +26,7 @@ BG = pygame.transform.scale(
 )
 
 # Define Player ATTRIBUTES
+PLAYER_HP = 5
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
 PLAYER_VEL = 5
@@ -28,8 +40,31 @@ STAR_VEL = 1
 FONT = pygame.font.SysFont("Verdana", 26)
 
 
+# Start Menu Draw Function
+def draw_start_menu():
+    WIN.fill((0, 0, 0))
+    font = pygame.font.SysFont("arial", 40)
+    title = font.render("My Game", True, (255, 255, 255))
+    start_button = font.render("Start", True, (255, 255, 255))
+    WIN.blit(
+        title,
+        (
+            WIDTH / 2 - title.get_width() / 2,
+            HEIGHT / 2 - title.get_height() / 2,
+        ),
+    )
+    WIN.blit(
+        start_button,
+        (
+            WIDTH / 2 - start_button.get_width() / 2,
+            HEIGHT / 2 + start_button.get_height() / 2,
+        ),
+    )
+    pygame.display.update()
+
+
 # Draw Function
-def draw(player: pygame.Rect, elapsed_time: float, astroids: list) -> None:
+def draw(player: pygame.Rect, elapsed_time: float, astroids: list, hp) -> None:
     # Draw Background Image
     WIN.blit(BG, (0, 0))
 
@@ -39,6 +74,10 @@ def draw(player: pygame.Rect, elapsed_time: float, astroids: list) -> None:
     # Elapsed Time
     time_text = FONT.render(f"Time: {round(elapsed_time, 1)}s", 1, "white")
     WIN.blit(time_text, (10, 10))
+
+    # Elapsed Time
+    time_text = FONT.render(f"Lives Left: {hp}/5", 1, "white")
+    WIN.blit(time_text, (10, 40))
 
     # Draw astroids
     for astroid in astroids:
@@ -54,7 +93,7 @@ def main() -> None:
     # Player
     player = pygame.Rect(
         WIDTH / 2, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT
-    )  # PLAYER will appear @ Bottom-Centre of Screen W/ HGHT of 60 && WDTH of 40
+    )  # PLAYER will appear @ Bottom-Centre of WIN W/ HGHT of 60 && WDTH of 40
 
     # CLOCK
     clock = pygame.time.Clock()
@@ -69,23 +108,13 @@ def main() -> None:
     astroids = []
 
     # Hit Check
-    hit = False
+    hp = PLAYER_HP
 
     # Main Game Loop
     while run:
         # Clock Ticking + astroid Generating
         star_count += clock.tick(60)  # 60 FPS
         elapsed_time = time.time() - start_time
-
-        # Generate astroids
-        if star_count > star_add_increment:
-            for _ in range(2):
-                star_x = random.randint(0, WIDTH - STAR_WIDTH)
-                astroid = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
-                astroids.append(astroid)
-
-            star_add_increment = max(200, star_add_increment - 50)
-            star_count = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -107,6 +136,16 @@ def main() -> None:
         if keys[pygame.K_s] and player.y + PLAYER_VEL + player.height <= HEIGHT:
             player.y += PLAYER_VEL  # Verticle Speed = 1/2 Horizontal Speed
 
+        # Generate astroids
+        if star_count > star_add_increment:
+            for _ in range(2):
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                astroid = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                astroids.append(astroid)
+
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
+
         # PROJECTILE MOVEMENT
         for astroid in astroids[:]:
             astroid.y += STAR_VEL
@@ -114,11 +153,10 @@ def main() -> None:
                 astroids.remove(astroid)
             elif astroid.y + astroid.height >= player.y and astroid.colliderect(player):
                 astroids.remove(astroid)
-                hit = True
+                hp -= 1
                 break
 
-        # Validating Hit
-        if hit:
+        if hp <= 0:
             lost_text = FONT.render("You Lost!", 1, "white")
             WIN.blit(
                 lost_text,
@@ -132,7 +170,7 @@ def main() -> None:
             break
 
         # Draw Elements
-        draw(player, elapsed_time, astroids)
+        draw(player, elapsed_time, astroids, hp)
     pygame.quit()
 
 
