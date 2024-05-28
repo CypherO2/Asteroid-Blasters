6eb4b71d2cedc510a9a95b1ed793002ad2f4efad
Cassi <120105652+CypherO2@users.noreply.github.com>
2024-05-28 20:10:04 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,140 @@
+import pygame
+import time
+import random
+
+# FONT INITIALISATION
+pygame.font.init()
+
+WIDTH, HEIGHT = 1600, 900
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Test Game 1")
+
+# Define Background Image
+BG = pygame.transform.scale(
+    pygame.image.load("./assets/Background-Image.jpg"), (WIDTH, HEIGHT)
+)
+
+# Define Player ATTRIBUTES
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+PLAYER_VEL = 5
+
+# Define Projectile ATTRIBUTES
+STAR_WIDTH = 30
+STAR_HEIGHT = 20
+STAR_VEL = 1
+
+# FONT
+FONT = pygame.font.SysFont("Verdana", 26)
+
+
+# Draw Function
+def draw(player: pygame.Rect, elapsed_time: float, astroids: list) -> None:
+    # Draw Background Image
+    WIN.blit(BG, (0, 0))
+
+    # Draw Player
+    pygame.draw.rect(WIN, "purple", player)  # CAN: string color OR RGB-color code
+
+    # Elapsed Time
+    time_text = FONT.render(f"Time: {round(elapsed_time, 1)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
+
+    # Draw astroids
+    for astroid in astroids:
+        pygame.draw.rect(WIN, "white", astroid)
+
+    pygame.display.update()
+
+
+# Main
+def main() -> None:
+    run = True
+
+    # Player
+    player = pygame.Rect(
+        WIDTH / 2, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT
+    )  # PLAYER will appear @ Bottom-Centre of Screen W/ HGHT of 60 && WDTH of 40
+
+    # CLOCK
+    clock = pygame.time.Clock()
+
+    # Start Time
+    start_time = time.time()
+    elapsed_time = 0
+
+    # Projectile Initialisation
+    star_add_increment = 2000
+    star_count = 0
+    astroids = []
+
+    # Hit Check
+    hit = False
+
+    # Main Game Loop
+    while run:
+        # Clock Ticking + astroid Generating
+        star_count += clock.tick(60)  # 60 FPS
+        elapsed_time = time.time() - start_time
+
+        # Generate astroids
+        if star_count > star_add_increment:
+            for _ in range(2):
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                astroid = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                astroids.append(astroid)
+
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        # Movement
+        keys = pygame.key.get_pressed()
+        # LEFT MOVEMENT
+        if keys[pygame.K_a] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        # RIGHT MOVEMENT
+        if keys[pygame.K_d] and player.x + PLAYER_VEL + player.width <= WIDTH:
+            player.x += PLAYER_VEL
+        # UP MOVEMENT
+        if keys[pygame.K_w] and player.y - PLAYER_VEL >= 0:
+            player.y -= PLAYER_VEL  # Verticle Speed = 1/2 Horizontal Speed
+        # DOWN MOVEMENT
+        if keys[pygame.K_s] and player.y + PLAYER_VEL + player.height <= HEIGHT:
+            player.y += PLAYER_VEL  # Verticle Speed = 1/2 Horizontal Speed
+
+        # PROJECTILE MOVEMENT
+        for astroid in astroids[:]:
+            astroid.y += STAR_VEL
+            if astroid.y > HEIGHT:
+                astroids.remove(astroid)
+            elif astroid.y + astroid.height >= player.y and astroid.colliderect(player):
+                astroids.remove(astroid)
+                hit = True
+                break
+
+        # Validating Hit
+        if hit:
+            lost_text = FONT.render("You Lost!", 1, "white")
+            WIN.blit(
+                lost_text,
+                (
+                    WIDTH / 2 - lost_text.get_width() / 2,
+                    HEIGHT / 2 - lost_text.get_height() / 2,
+                ),
+            )
+            pygame.display.update()
+            pygame.time.delay(7000)
+            break
+
+        # Draw Elements
+        draw(player, elapsed_time, astroids)
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
